4b0c76aa7e1b26b724f4a1ed31de2ed09e4634fb
Davide Laneri <davidelan@hotmail.com>
2024-11-05 07:08:42 +0000

    Create initial leaf visulalizer

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from app_pages.multipage import MultiPage
 
 # load pages scripts
 from app_pages.page_summary import page_summary_body
+from app_pages.page_leaf_visualizer import page_leaf_visualizer_body
 
 
 app = MultiPage(app_name="Cherry Leaf Mildew Detection")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
 app.add_page("Project Summary", page_summary_body)
+app.add_page("Leaves Visualiser", page_leaf_visualizer_body)
 
 
 app.run()  # Run the app
---
+++ app_pages/page_leaf_visualizer.py
@@ -0,0 +1,110 @@
+import streamlit as st
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+import itertools
+import random
+
+def page_leaf_visualizer_body():
+    st.write("### Leaves Visualizer")
+    st.info(
+        f"A study that visually differentiates a cherry leaf affected by powdery mildew from a healthy one.")
+
+    st.write(
+        f"For additional information, please visit and **read** the "
+        f"[Project README file](https://github.com/cla-cif/Detection-Cherry-Powdery-Mildew#readme).")
+
+    st.warning(
+        f"We suspect cherry leaves affected by powdery mildew have clear marks," 
+        f" typically the first symptom is a light-green, circular lesion on either leaf surface," 
+        f" then a subtle white cotton-like growth develops in the infected area.\n\n" 
+        f" This property has to be translated in machine learning terms," 
+        f" images have to be 'prepared' before being fed to the model for an optimal feature extraction and training.\n\n"
+        f" When we are dealing with an Image dataset, it's important to normalize the images in the dataset before training a Neural Network on it." 
+        f" To normalize an image, one will need the mean and standard deviation of the entire dataset that are calculated with a mathematical formula"
+        f" which takes into consideration the properties of an image"
+    )
+    
+    version = 'v1'
+    if st.checkbox("Difference between average and variability image"):
+      
+      avg_powdery_mildew = plt.imread(f"outputs/{version}/avg_var_powdery_mildew.png")
+      avg_uninfected = plt.imread(f"outputs/{version}/avg_var_healthy.png")
+
+      st.warning(
+        f"We notice the average and variability images did not show "
+        f"patterns where we could intuitively differentiate one from another. " 
+        f"However, mildew affected leaves show more white stipes on the center.")
+
+      st.image(avg_powdery_mildew, caption='Affected leaf - Average and Variability')
+      st.image(avg_uninfected, caption='healthy leaf - Average and Variability')
+      st.write("---")
+
+    if st.checkbox("Differences between average infected and average healthy leaves"):
+          diff_between_avgs = plt.imread(f"outputs/{version}/avg_diff.png")
+
+          st.warning(
+            f"We notice this study didn't show "
+            f"patterns where we could intuitively differentiate one from another.")
+          st.image(diff_between_avgs, caption='Difference between average images')
+
+    if st.checkbox("Image Montage"): 
+      st.write("To refresh the montage, click on the 'Create Montage' button")
+      my_data_dir = 'inputs/cherryleaves_dataset/cherry-leaves'
+      labels = os.listdir(my_data_dir+ '/validation')
+      label_to_display = st.selectbox(label="Select label", options=labels, index=0)
+      if st.button("Create Montage"):      
+        image_montage(dir_path= my_data_dir + '/validation',
+                      label_to_display=label_to_display,
+                      nrows=8, ncols=3, figsize=(10,25))
+      st.write("---")
+
+
+def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15,10)):
+  sns.set_style("white")
+  labels = os.listdir(dir_path)
+
+  # subset the class you are interested to display
+  if label_to_display in labels:
+
+    # checks if your montage space is greater than subset size
+    # how many images in that folder
+    images_list = os.listdir(dir_path+'/'+ label_to_display)
+    if nrows * ncols < len(images_list):
+      img_idx = random.sample(images_list, nrows * ncols)
+    else:
+      print(
+          f"Decrease nrows or ncols to create your montage. \n"
+          f"There are {len(images_list)} in your subset. "
+          f"You requested a montage with {nrows * ncols} spaces")
+      return
+    
+
+    # create list of axes indices based on nrows and ncols
+    list_rows= range(0,nrows)
+    list_cols= range(0,ncols)
+    plot_idx = list(itertools.product(list_rows,list_cols))
+
+
+    # create a Figure and display images
+    fig, axes = plt.subplots(nrows=nrows,ncols=ncols, figsize=figsize)
+    for x in range(0,nrows*ncols):
+      img = imread(dir_path + '/' + label_to_display + '/' + img_idx[x])
+      img_shape = img.shape
+      axes[plot_idx[x][0], plot_idx[x][1]].imshow(img)
+      axes[plot_idx[x][0], plot_idx[x][1]].set_title(f"Width {img_shape[1]}px x Height {img_shape[0]}px")
+      axes[plot_idx[x][0], plot_idx[x][1]].set_xticks([])
+      axes[plot_idx[x][0], plot_idx[x][1]].set_yticks([])
+    plt.tight_layout()
+    
+    st.pyplot(fig=fig)
+    # plt.show()
+
+
+  else:
+    print("The label you selected doesn't exist.")
+    print(f"The existing options are: {labels}")
