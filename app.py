6e9eb0ad429a1dbff99ed29ed660a49c233c06dc
Davide Laneri <davidelan@hotmail.com>
2024-11-10 17:18:48 +0000

    Create ML performance page

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from app_pages.page_summary import page_summary_body
 from app_pages.page_leaf_visualizer import page_leaf_visualizer_body
 from app_pages.page_mildew_detector import page_mildew_detector_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_ml_performance import page_ml_performance_body
+
+
 
 
 app = MultiPage(app_name="Cherry Leaf Mildew Detection")  # Create an instance of the app
@@ -15,6 +18,7 @@ app.add_page("Project Summary", page_summary_body)
 app.add_page("Leaves Visualiser", page_leaf_visualizer_body)
 app.add_page("Mildew Detector", page_mildew_detector_body)
 app.add_page("Project Hypothesis", page_project_hypothesis_body)
+app.add_page("Machine Learning Performance", page_ml_performance_body)
 
 
 app.run()  # Run the app
---
+++ app_pages/page_ml_performance.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.image import imread
+from src.machine_learning.evaluate_clf import load_test_evaluation
+
+
+def page_ml_performance_body():
+    version = 'v1'
+
+    st.header("ML Performance")
+    st.info(
+        f"This page provides a visually user-friendly illustration of how the dataset was divided "
+        f"for the analysis, model training losses and accuracy and"
+        f" a generalised performance on test set. ")
+
+    st.write("### Train, Validation and Test Set: Labels Frequencies")
+
+    labels_distribution = plt.imread(f"outputs/{version}/labels_distribution.png")
+    st.image(labels_distribution, caption='Labels Distribution on Train, Validation and Test Sets')
+    st.write("---")
+
+
+    st.subheader("Model Training Losses")
+    st.image(f"outputs/{version}/model_training_losses.png", caption='Training and Validation Loss')
+
+    st.subheader("Model Training Accuracy")
+    st.image(f"outputs/{version}/model_training_acc.png", caption='Training and Validation Accuracy')
+    st.write("---")
+
+    st.write("### Generalised Performance on Test Set")
+    st.dataframe(pd.DataFrame(load_test_evaluation(version), index=['Loss', 'Accuracy']))
+
+    
+    
+    
